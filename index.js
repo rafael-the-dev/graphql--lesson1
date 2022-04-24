2fa74632cfc138b66a4cffec1951189223700138
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-24 13:24:17 +0200

    define addBook mutation

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const { ApolloServer, gql } = require("apollo-server");
 
 const books = [
     {
-      title: 'The Awakening',
-      author: {
-          name: 'Kate Chopin',
-      }
+        title: 'The Awakening',
+        author: {
+            name: 'Kate Chopin',
+        }
     },
     {
-      title: 'City of Glass',
-      author: {
-          name: 'Paul Auster',
-      }
+        title: 'City of Glass',
+        author: {
+            name: 'Paul Auster',
+        }
     },
 ];
+
+const addBook = ({ authorName, bookTitle}) => {
+    books.push({
+        title: bookTitle,
+        author: {
+            name: authorName
+        }
+    });
+};
 
 const typeDefs = gql`
     type Book {
@@ -30,10 +39,20 @@ const typeDefs = gql`
         authors: [Author]
         books: [Book]
     }
+
+    type Mutation {
+        addBook(title: String, author: String): Book
+    }
     
 `;
 
 const resolvers = {
+    Mutation: {
+        addBook: (_, { title, author }) => {
+            addBook({ authorName: author, bookTitle: title });
+            return books[books.length - 1];
+        }
+    },
     Query: {
         books: () => books
     }
